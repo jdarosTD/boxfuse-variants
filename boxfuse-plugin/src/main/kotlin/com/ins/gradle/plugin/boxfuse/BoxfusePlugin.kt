60285e3bd75842a5a886de3555be3fffb2696760
Jonathan DA ROS <jdaros@telecomdesign.fr>
2019-06-13 11:08:33 +0200

    removing unsused code + generation of task for each boxfuse env

--- boxfuse-plugin/src/main/kotlin/com/ins/gradle/plugin/boxfuse/BoxfusePlugin.kt
+++ boxfuse-plugin/src/main/kotlin/com/ins/gradle/plugin/boxfuse/BoxfusePlugin.kt
@@ -1,13 +1,19 @@
 package com.ins.gradle.plugin.boxfuse
 
+import com.boxfuse.client.gradle.BoxfuseExtension
 import com.boxfuse.client.gradle.task.RunTask
 import groovy.lang.Closure
 import org.gradle.api.Action
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.model.ObjectFactory
 import org.gradle.internal.reflect.Instantiator
 import javax.inject.Inject
+import org.gradle.api.tasks.Nested
+
+
+
 
 
 /**
@@ -16,28 +22,54 @@ import javax.inject.Inject
 class BoxfusePlugin @Inject constructor(val instantiator: Instantiator) : Plugin<Project> {
 
 
+    private val PLUGIN_EXTENSION_NAME = "boxFuse"
 
     override fun apply(project: Project) {
         // Create the 'greeting' extension
 
         // Create a container of Book instances
-        val persons = project.container(Variant::class.java)
+        val extension = project.extensions.create(PLUGIN_EXTENSION_NAME, PluginExtension::class.java)
+        extension.variants = project.container(Variant::class.java)
+        extension.config = BoxfuseExtension()
 
-        project.extensions.add("boxFuse", persons)
+        var boxfuseExtension = project.extensions.findByName("boxfuse") as BoxfuseExtension?
 
-        persons.whenObjectAdded{ variant ->
-            project.tasks.create(
-                "boxFuse" +
-                        variant.name.capitalize() + variant.env?.capitalize(), RunTask::class.java) {
 
-                it.app= variant.name
-                it.env= variant.env
 
-                it.doLast {
-                    println("Deployed ${variant.name} to ${variant.env}")
+        project.afterEvaluate{
+            val ext =project.extensions.findByName("boxFuse") as PluginExtension
+
+            if(boxfuseExtension == null){
+                project.extensions.add("boxfuse",ext.config)
+            }
+            ext.variants?.all{ variant ->
+
+                variant.config = BoxfuseExtension()
+                val envs = arrayOf("test","prod", "dev")
+                for(env in envs){
+                    project.tasks.create(
+                        "boxFuse" +
+                                variant.name.capitalize() + env?.capitalize(), RunTask::class.java) {task ->
+
+                        task.app= variant.name
+                        task.env= env
+                        task.payload =  variant.config?.payload?:ext.config?.payload
+                        task.secret =  variant.config?.secret?:ext.config?.secret
+                        task.user =  variant.config?.user?:ext.config?.user
+
+
+                        task.doFirst{
+                            println("Deploying ${variant.name} to $env with payload : ${task.payload}, user ${task.user} - secret ${task.secret}");
+                        }
+                        task.doLast {
+                            println("${variant.name} is deployed to $env")
+                        }
+                    }
                 }
+
             }
         }
+
 
     }
 
@@ -50,12 +82,29 @@ open class Variant constructor(var name : String){
     fun env(closure : Closure<String>){
         closure.call(env)
     }
-}
-open class GreetingPluginExtension @javax.inject.Inject constructor(objectFactory: ObjectFactory) {
-    var message: String? = null
-    val greeter: Variant = objectFactory.newInstance(Variant::class.java)
+    var config : BoxfuseExtension?= null
 
-    fun greeter(action: Action<in Variant>) {
-        action.execute(greeter)
+    fun config( closure : Closure<BoxfuseExtension>) {
+        org.gradle.util.ConfigureUtil.configure(closure, config)
     }
+
+}
+open class PluginExtension @javax.inject.Inject constructor() {
+
+
+    var config : BoxfuseExtension?= null
+    var variants : NamedDomainObjectContainer<Variant>?= null
+    var payload : String?= null
+
+    fun config( closure : Closure<BoxfuseExtension>) {
+        org.gradle.util.ConfigureUtil.configure(closure, config)
+    }
+//    fun config(action: Action<in BoxfuseExtension?>) {
+//        action.execute(config)
+//    }
+
+    fun variants( configuration : Closure<Variant>) {
+        variants?.configure(configuration)
+    }
+
 }
